d69fcf396186f0e3c8a326e8d4600de9f50710a4
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-04-27 22:22:01 +0300

    edit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,18 +6,18 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i = 0, j = 0;
-    int tmp = 0;
+	size_t i = 0, j = 0;
+	int tmp = 0;
 
-    for(j = 0; j < size; j++)
-    for(i = 0; i < size - 1; i++)
-    {
-        if (array[i] > array[i + 1])
-        {
-            tmp = array[i + 1];
-            array[i + 1] = array[i];
-            array[i] = tmp;
-        }
-        print_array(array, size);
-    }
+	for (j = 0; j < size; j++)
+	for (i = 0; i < size - 1; i++)
+	{
+		if (array[i] > array[i + 1])
+		{
+			tmp = array[i + 1];
+			array[i + 1] = array[i];
+			array[i] = tmp;
+		}
+		print_array(array, size);
+	}
 }
