6c18f9259821cad9878d5a38657487e15576fdb4
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-04-28 22:41:13 +0300

    edit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,47 @@
 #include "sort.h"
 /**
  * swap - swap
- * @befor: first node
+ * @before: first node
  * @after: second node
  */
 void swap(listint_t *before, listint_t *after)
 {
-    if (after->next != NULL)
-        after->next->prev = before;
-    if (before->prev != NULL)
-        before->prev->next = after;
-            
-    before->next = after->next;
-    after->prev = before->prev;
-    before->prev = after;
-    after->next = before;
+	if (after->next != NULL)
+		after->next->prev = before;
+	if (before->prev != NULL)
+		before->prev->next = after;
+
+	before->next = after->next;
+	after->prev = before->prev;
+	before->prev = after;
+	after->next = before;
 }
 /**
- * insertion_sort_list - insertion sort linked list (brings the smallest element at the beginning)
+ * insertion_sort_list - insertion sort linked list (smallest beginning)
  * @list: double linked list to be sorted
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *key, *checker;
+	listint_t *key, *checker;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
-    key = (*list)->next;
-    while (key != NULL)
-    {
-        checker = key;
-        key = key->next;
-        while(checker && checker->prev)
-        {
-            if (checker->n < checker->prev->n)
-            {
-                swap(checker->prev, checker);
-                if (checker->prev == NULL)
-                    *list = checker;
-                print_list((const listint_t *)*list);
-            }
-            else
-                checker = checker->prev;
-        }
-    }
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	key = (*list)->next;
+	while (key != NULL)
+	{
+		checker = key;
+		key = key->next;
+		while (checker && checker->prev)
+		{
+			if (checker->n < checker->prev->n)
+			{
+				swap(checker->prev, checker);
+				if (checker->prev == NULL)
+					*list = checker;
+				print_list((const listint_t *)*list);
+			}
+			else
+				checker = checker->prev;
+		}
+	}
 }
